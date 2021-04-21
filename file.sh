4d0df8c699903f56ba4f6ad80cfcd6bb0b8d965b
tsy7 <82978748+tsy7@users.noreply.github.com>
2021-04-22 00:55:41 +0300

    Add files via upload

---
+++ file.sh
@@ -0,0 +1,9 @@
+pip3 install requests
+pip3 install json
+pip3 install codecs
+pip3 install readline
+pip3 install ftplib 
+pip3 install colorama 
+pip3 install urllib.request
+pip3 install urllib2
+
